d21dc7c203cf8d5e6066537bdc434418e65ef126
Ekojoecovenant <covenantekojoe@gmail.com>
2025-08-13 15:50:40 +0100

    Update CourseServiceImpl.java

--- learning_api/src/main/java/com/niit/mms/learning_api/service/CourseServiceImpl.java
+++ learning_api/src/main/java/com/niit/mms/learning_api/service/CourseServiceImpl.java
@@ -5,6 +5,7 @@ import com.niit.mms.learning_api.repository.CourseRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CourseServiceImpl implements CourseService {
@@ -22,13 +23,26 @@ public class CourseServiceImpl implements CourseService {
 
     @Override
     public Course updateCourse(Long id, Course course) {
-        return courseRepository.findById(id).map(existing -> {
+        Optional<Course> existingCourse = courseRepository.findById(id);
 
-
-
-                    return courseRepository.save(existing);
-                }
-        ).orElse(null);
+        if(existingCourse.isPresent()) {
+            Course updatedCourse = existingCourse.get();
+            updatedCourse.setDescription(updatedCourse.getDescription());
+            updatedCourse.setDuration(updatedCourse.getDuration());
+            updatedCourse.setTitle(updatedCourse.getTitle());
+            // Add other fields as needed
+            return courseRepository.save(updatedCourse);
+        }
+        throw new RuntimeException("Course not found with id: " + id);
+//        return courseRepository.findById(id).map(existing -> {
+//
+//                    existing.setDescription(existing.getDescription());
+//                    existing.setDuration(existing.getDuration());
+//                    existing.setTitle(existing.getTitle());
+//
+//                    return courseRepository.save(existing);
+//                }
+//        ).orElse(null);
     }
 
     @Override
